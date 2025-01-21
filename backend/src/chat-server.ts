28ff22114692ccee13cd4d15e7ba38e41f49a7c7
hhow09 <hhow09@gmail.com>
2025-01-22 00:40:35 +0100

    feat: setup socket io server - manual test with test_index.html

--- backend/src/chat-server.ts
+++ backend/src/chat-server.ts
@@ -1,27 +1,49 @@
 import express, { Request, Response } from 'express';
 import { Logger } from "pino";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
 
 export class ChatServer {
     port: number;
     app: express.Application;
     logger: Logger;
+    io: Server;
+
     constructor(logger: Logger, port: number = 3000) {
         this.port = port;
         this.logger = logger;
         this.app = express();
+        // ref: https://socket.io/docs/v4/server-initialization/#with-express
+        this.io = new Server(createServer(this.app), {
+            cors: {
+                origin: '*',
+            }
+        });
         this.setupAppRoutes();
+        this.setupSocketHandler();
     }
     setupAppRoutes() {
         // health check
-        // https://kubernetes.io/docs/reference/using-api/health-checks/
+        // ref: https://kubernetes.io/docs/reference/using-api/health-checks/
         this.app.get('/livez', (req: Request, res: Response) => {
             res.send('OK');
         });
     }
 
-    listen() {
-        this.app.listen(this.port, () => {
+    setupSocketHandler() {
+        this.io.on("listening", () => {
             this.logger.info(`Server running at http://localhost:${this.port}`);
         });
+        this.io.on('connection', (socket: Socket) => {
+            const sessionLogger = this.logger.child({ session: socket.id });
+            sessionLogger.info(`Connected client session ${socket.id} on port ${this.port}`);
+            socket.on('disconnect', () => {
+                sessionLogger.info('a client disconnected');
+            });
+        });
+    }
+
+    listen() {
+        this.io.listen(this.port);
     }
 }
