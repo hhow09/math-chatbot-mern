63a3eb6745d6d9a772a0390370d5089eced065c9
hhow09 <hhow09@gmail.com>
2025-01-22 00:45:21 +0100

    feat: setup eslint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {ignores: ["dist/**"]},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
