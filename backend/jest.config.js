56df1ee2073741658a4fd3b054d0c2fd983ee12b
hhow09 <hhow09@gmail.com>
2025-01-22 00:42:37 +0100

    feat: setup jest

---
+++ backend/jest.config.js
@@ -0,0 +1,12 @@
+// https://kulshekhar.github.io/ts-jest/docs/26.5/getting-started/installation
+// Steps: 
+// npm install --save-dev jest typescript ts-jest @types/jest
+// npx ts-jest config:init
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+    testEnvironment: "node",
+    transform: {
+      "^.+.tsx?$": ["ts-jest",{}],
+    },
+  };
+  
