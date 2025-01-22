e4bd65425dfd2064f13011a06c07801da72d71d4
hhow09 <hhow09@gmail.com>
2025-01-23 00:09:07 +0100

    fix: mongo connection async

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,15 +9,20 @@ const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
 const dbName = "math-chat";
 const collectionName = "chat-session";
 
-const logger = pino();
-const collection = getMongoCollection(uri, dbName, collectionName);
-const commandService = new CommandService(logger, new ChatRepo(collection));
-const chatServer = new ChatServer(logger, commandService, 3000);
-chatServer.listen();
-
-function getMongoCollection(uri: string, dbName: string, collectionName: string): Collection<ChatSession> {
+async function getMongoCollection(uri: string, dbName: string, collectionName: string): Promise<Collection<ChatSession>> {
     const mongoClient = new MongoClient(uri);
+    await mongoClient.connect();
     const db = mongoClient.db(dbName);
     const collection = db.collection<ChatSession>(collectionName);
     return collection;
 }
+
+async function main() {
+    const logger = pino();
+    const collection = await getMongoCollection(uri, dbName, collectionName);
+    const commandService = new CommandService(logger, new ChatRepo(collection));
+    const chatServer = new ChatServer(logger, commandService, 3000);
+    chatServer.listen();
+}
+
+main();
